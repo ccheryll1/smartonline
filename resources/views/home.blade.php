5ad2d5cba67d3785ecddc7bd1ca04fcb5307d8e5
ccheryll1 <rylche663@gmail.com>
2025-04-27 11:47:31 +0700

    panjang button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,7 +43,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
             <div class="flex flex-col items-center">
                 <img src="/image/image.png" alt="Gambar" class="w-full h-auto rounded-lg shadow-lg">
-                <a href="#" class="mt-6 bg-white text-blue-500 hover:bg-blue-500 hover:text-white px-10 py-3 rounded-full shadow-lg hover:shadow-2xl transition-all duration-300">
+                <a href="#" class="mt-6 bg-white text-blue-500 hover:bg-blue-500 hover:text-white px-25 py-3 rounded-full shadow-lg hover:shadow-2xl transition-all duration-300">
                     Request Demo
                 </a>
             </div>
