3955dbb6a3f30198d0f4b60f9805337d2288cb4a
RanuPutra <raya.ranuputra@gmail.com>
2025-04-29 11:47:09 +0700

    idk what is this just adding /

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Time Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
 <body>
     <div class="d-flex">
@@ -15,7 +16,7 @@
                 <hr>
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a href="/" class="nav-link active">Attendance</a>
+                        <a href="/attendances" class="nav-link active">Attendance</a>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">Overtime</a>
