7493cdff513ec2ad5e02e0567ad6928bc20a3d1f
ccheryll1 <rylche663@gmail.com>
2025-04-29 16:11:37 +0700

    solusi1

--- resources/views/solusi.blade.php
+++ resources/views/solusi.blade.php
@@ -8,21 +8,25 @@
 </head>
 
 <body>
-       <div class="bg-blue-100 min-h-screen">
-              <div>
-                     <h1> Make Better Collaboration Achieve Productivity in Just One Place</h1>
-                     <p> Satu aplikasi yang berfungsi sebagai media informasi, komunikasi, kolaborasi hingga persoonalia perusahaan untuk menunjang produktivitas karyawan dalam perusahaan. </p>
-              </div>
-              <div>
-                     <img src="" alt="">
-              </div>
-              <div>
-                     <h1> Smart Accounting </h1>
-                            <p> aplikasi accounting untuk kebutuhan perusahaan anda</p>
-                     <h1> Smart HRIS </h1>
-                            <p> aplikasi HRIS untuk kebutuhan perusahaan anda</p>
-                     <h1> Smart POS </h1>
-                            <p> aplikasi HRIS untuk kebutuhan perusahaan anda</p>
+       <div class="bg-blue-100 min-h-screen py-15 px-25">
+              <div class="flex items-center justify-between gap-10 mt-5">
+                     <div class="w-1/2 ">
+                            <div class="mt-10">
+                                   <h1 class="text-[35px] text-black font-bold  w-160"> Make Better Collaboration, Achieve Productivity in Just One Place</h1>
+                                   <p class="text-lg text-neutral-500 w-160 mt-3"> Satu aplikasi yang berfungsi sebagai media informasi, komunikasi, kolaborasi hingga persoonalia perusahaan untuk menunjang produktivitas karyawan dalam perusahaan. </p>
+                            </div>
+                            <div class="bg-violet-200 mt-10 py-8 px-16 w-130 rounded-4xl">
+                                   <h1 class="text-2xl font-semibold"> Smart accounting </h1>
+                                          <p> aplikasi accounting untuk kebutuhan perusahaan anda</p>
+                                   <h1 class="text-2xl font-semibold mt-3"> Smart HRIS </h1>
+                                          <p> aplikasi HRIS untuk kebutuhan perusahaan anda</p>
+                                   <h1 class="text-2xl font-semibold mt-3"> Smart POS </h1>
+                                          <p> aplikasi HRIS untuk kebutuhan perusahaan anda</p>
+                            </div>
+                     </div>
+                     <div class="w-1/2 bg-blue-900/85 rounded-2xl">
+                            <img src="/image/imageharga.png" alt="" class="w-full h-auto">
+                     </div>
               </div>
        </div>
 </body>
