9ce589055de7e1cf6a9c67e32d312bd9667f5c80
ccheryll1 <rylche663@gmail.com>
2025-04-30 15:51:23 +0700

    ALAHHH LANJUT NANTI AJA

--- resources/views/solusi.blade.php
+++ resources/views/solusi.blade.php
@@ -11,12 +11,12 @@
        <div class="bg-blue-100 min-h-screen py-15 px-25">
               <div class="flex items-center justify-between gap-10 mt-5">
                      <div class="w-1/2 ">
-                            <div class="mt-10">
+                            <div class="mt-5">
                                    <h1 class="text-[35px] text-black font-semibold  w-160"> Make Better Collaboration, Achieve Productivity in Just One Place</h1>
-                                   <p class="text-lg text-neutral-500 w-160 mt-3"> Satu aplikasi yang berfungsi sebagai media informasi, komunikasi, kolaborasi hingga persoonalia perusahaan untuk menunjang produktivitas karyawan dalam perusahaan. </p>
+                                   <p class="text-lg text-neutral-600 w-160 mt-3"> Satu aplikasi yang berfungsi sebagai media informasi, komunikasi, kolaborasi hingga persoonalia perusahaan untuk menunjang produktivitas karyawan dalam perusahaan. </p>
                             </div>
-                            <!-- <div class="bg-violet-200 mt-10 py-8 px-16 w-130 rounded-4xl"> -->
-                            <div class="outline-3 outline-offset-2 outline-solid mt-7 py-3 px-16 w-150 rounded-4xl">
+                            <div class="bg-violet-200 mt-3 py-3 px-16 w-130 rounded-4xl">
+                            <!-- <div class="outline-3 outline-offset-2 outline-solid mt-7 py-3 px-16 w-150 rounded-4xl"> -->
                                    <h1 class="text-2xl font-semibold"> Smart accounting </h1>
                                           <p> aplikasi accounting untuk kebutuhan perusahaan anda</p>
                                    <h1 class="text-2xl font-semibold mt-3"> Smart HRIS </h1>
@@ -31,8 +31,26 @@
               </div>
        </div>
 
-       <div class="bg-blue-900 min-h-screen py-15 px-25">
-
+       <div class="bg-green-100 min-h-screen py-15 px-25">
+              <!-- <div class="w-1/2 rounded-2xl">
+                     <img src="/image/solusi2.png" alt="" class="w-full h-auto">
+              </div> -->
+              <div >
+                     <div>
+                            <h1 class="text-4xl font-semibold text-blue-950"> Kemudahan Akses Informasi Perusahaan dalam Satu Genggaman </h1>
+                            <p class="text-lg text-neutral-600"> Hanya butuh satu aplikasi untuk kantor. Dan seluruh fungsi komunikasi dan kolaborasi pekerjaan menjadi lebih mudah.</p>
+                     </div>
+                     <div>
+                            <h1 class="text-2xl font-semibold"> Company Profile </h1>
+                                   <p> Sosialisasi profil dan visi misi perusahaan lebih mudah diakses seluruh internal perusahaan. </p>
+                            <h1 class="text-2xl font-semibold"> Board of Director </h1>
+                                   <p> Mengenal profil BOD dan jajaran stakeholder perusahaan cukup dalam satu halaman di aplikasi. </p>
+                            <h1 class="text-2xl font-semibold"> Employee Directory </h1>
+                                   <p> Akses direktori seluruh karyawan tanpa menambahkan kontak dan nomor HP. Memudahkan internal perushaan untuk berkomunikasi satu sama lain dengan tetap menjaga privasi masing-masing. </p>
+                            <h1 class="text-2xl font-semibold"> Document Management </h1>
+                                   <p>  Miliki file sharing sendiri untuk kemudahan akses dan dokumen-dokumen perusahaan untuk seluruh karyawan. </p>
+                     </div>
+              </div>
        </div>
 
 </body>
