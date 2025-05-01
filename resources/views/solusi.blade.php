019eac82069d9260545fa2126e9bdfc8a19a5a65
ccheryll1 <rylche663@gmail.com>
2025-05-02 01:48:36 +0700

    solusi 2,3. bg cuma jd penanda yh

--- resources/views/solusi.blade.php
+++ resources/views/solusi.blade.php
@@ -70,27 +70,36 @@
                             <div>
                                    <h3 class="text-xl text-blue-400 mb-2"> INTERNAL MESSAGING </h3>
                                    <h1 class="text-4xl font-semibold text-blue-950 mb-3"> Platform Chat yang Dirancang Khusus untuk Kolaborasi Internal </h1>
-                                   <p class="text-lg text-neutral-600"> Hanya butuh satu aplikasi untuk kantor. Dan seluruh fungsi komunikasi dan kolaborasi pekerjaan menjadi lebih mudah.</p>
+                                   <!-- <p class="text-lg text-neutral-600"> Hanya butuh satu aplikasi untuk kantor. Dan seluruh fungsi komunikasi dan kolaborasi pekerjaan menjadi lebih mudah.</p> -->
                             </div>
-                            <div class="grid grid-cols-2 gap-6 mt-6">
+                            <div class="grid grid-flow-col grid-rows-3  mt-6">
                                    <div class="max-w-md mx-auto">  
-                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Company Profile </h1>
-                                          <p class="text-center text-neutral-600 "> Sosialisasi profil dan visi misi perusahaan lebih mudah diakses seluruh internal perusahaan. </p>
+                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Personal Chat </h1>
+                                          <p class="text-center text-neutral-600 "> Percakapan personal dengan kemudahan akses kontak seluruh karyawan perusahaan. </p>
                                    </div>
                                    <div class="max-w-md mx-auto">
-                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Board of Director </h1>
-                                          <p class="text-center text-neutral-600"> Mengenal profil BOD dan jajaran stakeholder perusahaan cukup dalam satu halaman di aplikasi. </p>
+                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Group Chat </h1>
+                                          <p class="text-center text-neutral-600"> Fitur official group chat berdasarkan departmen mauupun buat grup sendiri secara fleksibel. </p>
                                    </div>
-                                   <div class="mt-3 max-w-md mx-auto">
-                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Employee Directory </h1>
-                                          <p class="text-center text-neutral-600"> Akses direktori seluruh karyawan tanpa menambahkan kontak dan nomor HP. Memudahkan internal perushaan untuk berkomunikasi satu sama lain dengan tetap menjaga privasi masing-masing. </p>
+                                   <div class="max-w-md mx-auto">
+                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Chat Bot </h1>
+                                          <nal class="text-center text-neutral-600"> Optimalkan fungsi percakapan internal perusahaan dengan menggunakan automasi chat bot yang bisa disesuaikan dengan kebutuhan perusahaan. </p>
                                    </div>
-                                   <div class="mt-3 max-w-md mx-auto">
-                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Document Management </h1>
-                                          <p class="text-center text-neutral-600">  Miliki file sharing sendiri untuk kemudahan akses dan dokumen-dokumen perusahaan untuk seluruh karyawan. </p>
+                                   <div class="max-w-md mx-auto">
+                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Sticker </h1>
+                                          <p class="text-center text-neutral-600">  Sampaikan ekspresi dan reaksi lewat stiker yang bisa dicustom sesuai kebutuhan dan branding perusahaan. </p>
                                    </div>
+                                   <div class="max-w-md mx-auto">
+                                          <h1 class="mb-2 text-center text-2xl font-semibold text-blue-950"> Broadcast Messages </h1>
+                                          <p class="text-center text-neutral-600">  Miliki channel broadcast chat sendiri untuk menyebarkan informasi satu arah. Atur, pilih dan setting penerima berdasarkan struktur perusahaan, jabatan, rule ataupun sesuai kebutuhan perusahaan tanpa biaya tambahan. </p>
+                                   </div>
+
                             </div>
                      </div>
+                     <div class="w-130 rounded-2xl bg-transparent">
+                            <img src="/image/solusi3.png" alt="" class="w-full h-auto">
+                     </div>
+
               </div>
        </div>
 
