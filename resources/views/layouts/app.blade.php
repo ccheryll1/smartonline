a8f319d70d9fbe425eb5846b12ba7a0fce3ee117
RanuPutra <raya.ranuputra@gmail.com>
2025-05-01 23:19:48 +0700

    CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Impor Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Roboto:wght@400&display=swap" rel="stylesheet">
+
+    
     <style>
         .card {
             border-radius: 10px;
